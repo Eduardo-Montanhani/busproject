0e85cc133af31f86a0fc89f6ebd365472464b1de
Eduardo-Montanhani <montanhani81@gmail.com>
2025-02-05 00:09:07 -0300

    mudando migrate poltronas

--- laravel/database/migrations/2025_01_31_131218_create_poltronas_table.php
+++ laravel/database/migrations/2025_01_31_131218_create_poltronas_table.php
@@ -3,19 +3,20 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('poltronas', function (Blueprint $table) {
             $table->id();
-            $table->integer('numero')->unique(); // Número único para cada poltrona
-            $table->foreignId('usuario_id')->nullable()->constrained('usuarios')->nullOnDelete(); // Associa a um usuário e remove a relação se o usuário for deletado
+            $table->integer('numero'); // Número da poltrona
+            $table->string('onibus')->nullable(); // Ônibus associado
+            $table->foreignId('usuario_id')->nullable()->constrained('usuarios')->nullOnDelete();
             $table->timestamps();
+
+            // Chave única combinando numero e onibus
+            $table->unique(['numero', 'onibus'], 'poltronas_numero_onibus_unique');
         });
     }
 
